a80663d2fc89674d898fcf34133eb5a3ed989ffc
willow-92 <75376443+willow-92@users.noreply.github.com>
2021-12-28 22:14:57 +0900

    Add files via upload

--- python_quiz_300.py
+++ python_quiz_300.py
@@ -1,3 +1,99 @@
+# 080 range 함수
+# 1 부터 99까지의 정수 중 짝수만 저장된 튜플을 생성하라.
+# 이 방법이 아니었음
+# i = 0
+# num = []
+# while i<98:
+#     i = i+2
+#     num.append(i)
+#     print(i)
+# num2 = tuple(num)
+# print(num2)
+# print(type(num2))
+
+# (2, 4, 6, 8 ... 98)
+
+
+# data = tuple(range(2,100,2))
+# print(data)
+
+
+# # 079 튜플 언팩킹
+# # 다음 코드의 실행 결과를 예상하라.
+
+# temp = ('apple', 'banana', 'cake')
+# a, b, c = temp
+# # a, b, c 각각의 위치와 상응하는 temp내의 원소가 들어감.
+# print(a, b, c)
+
+# 예상 답안 = apple, banana, cake
+
+# # 077
+# # 다음 튜플을 리스트로 변환하라.
+
+# interest = ('삼성전자', 'LG전자', 'SK Hynix')
+# interest = ['삼성전자', 'LG전자', 'SK Hynix']
+# print(type(interest))
+
+# # 또는
+# interest = ('삼성전자', 'LG전자', 'SK Hynix')
+# interest2 = list(interest)
+# print(type(interest2))
+
+# 076
+# 변수 t에는 아래와 같은 값이 저장되어 있다. 변수 t가 ('A', 'b', 'c') 튜플을 가리키도록 수정 하라.
+
+# t = ('a', 'b', 'c')
+# t = ('A', 'b', 'c')
+# print(t)
+
+# 튜플은 한 번 정의된 이후 원소를 변경할 수 없으므로 새로운 튜플을 만들어야 한다. 
+
+# # 075
+# # 아래와 같이 t에는 1, 2, 3, 4 데이터가 바인딩되어 있다. t가 바인딩하는 데이터 타입은 무엇인가?
+
+# t = 1, 2, 3, 4
+# # 정답: 튜플
+# # 원칙적으로 튜플은 괄호와 함께 데이터를 정의해야 하지만, 사용자 편의를 위해 괄호가 없이 동작하는 것 또한 가능하다. 
+
+# # 074
+# # 다음 코드를 실행해보고 오류가 발생하는 원인을 설명하라.
+
+# t = (1, 2, 3)
+# t[0] = 'a'
+# # Traceback (most recent call last):
+# #   File "<pyshell#46>", line 1, in <module>
+# #     t[0] = 'a'
+# # TypeError: 'tuple' object does not support item assignment
+
+# # 튜플은 원소의 값을  변경할 수 없기 때문.
+
+
+# # 073
+# # 숫자 1 이 저장된 튜플을 생성하라.
+
+# num = (1,)
+# print(num)
+# print(type(num))
+# # 하나의 데이터가 저장되는 경우 마지막에 쉼표를 입력해 주어야 함.
+
+# # 072
+# # 2016년 11월 영화 예매 순위 기준 top3는 다음과 같다. 영화 제목을 movie_rank 이름의 튜플에 저장하라. (순위 정보는 저장하지 않는다.)
+
+# # 순위	영화
+# # 1	닥터 스트레인지
+# # 2	스플릿
+# # 3	럭키
+
+# movie_rank = ('닥터 스트레인지', '스플릿', '럭키')
+# print(movie_rank)
+# print(type(movie_rank))
+
+# # 071
+# # my_variable 이름의 비어있는 튜플을 만들라.
+# my_variable = ()
+# print(type(my_variable))
+
 # # 060
 # # 다음 리스트의 평균을 출력하라.
 

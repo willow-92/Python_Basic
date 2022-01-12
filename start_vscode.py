d1ae1c0dff713b263ca6010860ac1e636e581ee7
willow-92 <75376443+willow-92@users.noreply.github.com>
2022-01-12 23:58:19 +0900

    daily commit

--- start_vscode.py
+++ start_vscode.py
@@ -1,14 +1,60 @@
-import subprocess
-subprocess.call(["D:\\Users\\anseilen\\Downloads\\key_macro (2).exe"])
-print('파일 불러오기를 완료했습니다')
+# 예시 1
+# import subprocess 
+# subprocess.call(["C:\\temp\\calc.exe"])
 
-import pyautogui
-print('라이브러리를 불러왔습니다.')
+# 예시 2
+# import os 
+# os.system('"C:/Windows/System32/notepad.exe"')
+
+# import subprocess
+# print('subprocess를 불러왔습니다.')
+# subprocess.call(["C:\\WINDOWS\\system32\\cmd.exe"])
+# print('파일 불러오기를 완료했습니다')
+
+# import os
+# os.system('"C:/WINDOWS/system32/cmd.exe"')
+# print('불러왔는지 확인')
+
+import pyautogui as pag
+import time
+# print('라이브러리를 불러왔습니다.')
 import pygetwindow as gw 
-print('pyget window 라이브러리를 불러왔습니다.')
-win = gw.getWindowsWithTitle('키보드')[0] 
-win.activate()
-pyautogui.press('f11')
-print('f11을 눌렀습니다')
-pyautogui.press('capslock')
-print('capslock을 눌렀습니다')
+# print('pyget window 라이브러리를 불러왔습니다.')
+# win = gw.getWindowsWithTitle('키보드')[0] 
+# win.activate()
+# win = gw.getWindowsWithTitle('명령 프롬프트')[1] 
+# win.activate()
+
+def cd_b():
+    pag.typewrite("cd..")
+    pag.press('enter')  
+
+def cd():
+    pag.typewrite("cd")
+    pag.press('enter')  
+
+pag.press('win')
+pag.typewrite("cmd")
+time.sleep(0.5)
+pag.press('enter')
+time.sleep(0.5)
+cd_b()
+cd_b()
+pag.typewrite("cd py")
+pag.press('tab')
+pag.press('enter')
+pag.typewrite("cd sc")
+pag.press('tab')
+time.sleep(0.5)
+pag.press('enter')
+pag.typewrite("act")
+pag.press('tab')
+time.sleep(0.5)
+pag.press('enter')
+time.sleep(0.5)
+pag.typewrite("code")
+pag.press('enter')
+
+# print('f11을 눌렀습니다')
+# pyautogui.press('capslock')
+# print('capslock을 눌렀습니다')
--- strat_vscode_in_venv.md
+++ strat_vscode_in_venv.md
@@ -6,31 +6,21 @@
 - 가장 익숙한 형태인 macro를 사용하여 cmd에서 가상환경 내 vscode를 실행하도록 세팅 해 두었음
 - 파이썬에서 macro를 실행하도록 코드를 작성
 
-```
-    import subprocess
-    subprocess.call(["D:\\Users\\anseilen\\Downloads\\key_macro (2).exe"])
-```
+'''
+import subprocess
+subprocess.call(["D:\\Users\\anseilen\\Downloads\\key_macro (2).exe"])
+'''
 
 이후 pyautogui 라이브러리를 사용하여 방금 만든 macro를 실행시키려고 시도함
 그러나 동작하지 않음
 
-```
-    import pyautogui
-    import pygetwindow as gw 
-    win = gw.getWindowsWithTitle('키보드') 
-    win.activate()
-    pyautogui.press('f11')
-    pyautogui.press('capslock')
-```
+'''
+import pyautogui
+import pygetwindow as gw 
+win = gw.getWindowsWithTitle('키보드') 
+win.activate()
+pyautogui.press('f11')
+pyautogui.press('capslock')
+'''
 
 관련 내용은 다른 기회에 확인이 필요함
-
-```
-time. sleep(숫자)
-```
-해당 코드를 통해서 시간 지연을 추가할 수 있음. 타임 라이브러리는 따로 불러와야 함
-키보드 입력 방식으로 진행이 어려울 경우 마우스 클릭하는 방법 또한 고려해 볼 필요가 있음
-
-다른 방식을 찾아보다가 성공
-리스트 파일 형태로 [0]을 입력해주었어야 했는데 빠져 있어서 발생한 문제
-내일은 스케쥴러에 파이썬 스크립트를 실행하도록 넣어주고 추가 테스트 진행해볼 필요가 있음.

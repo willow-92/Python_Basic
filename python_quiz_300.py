221ed62d34ad0ab76551c02675ae88a733c45d24
willow-92 <75376443+willow-92@users.noreply.github.com>
2022-02-24 08:11:33 +0900

    daily_commit

--- python_quiz_300.py
+++ python_quiz_300.py
@@ -1,3 +1,160 @@
+# 210
+# 아래 코드의 실행 결과를 예측하라.
+
+# def message1():
+#     print("A")
+
+# def message2():
+#     print("B")
+
+# def message3():
+#     for i in range (3) :
+#         message2()
+#         print("C")
+#     message1()
+
+# message3()
+
+
+'''
+B
+C
+B
+C
+B
+C
+A
+'''
+
+
+# # 209
+# # 아래 코드의 실행 결과를 예측하라.
+
+# # def message1():
+# #     print("A")
+
+# # def message2():
+# #     print("B")
+# #     message1()
+
+# # message2()
+
+
+# '''
+# B
+# A
+# '''
+
+
+# # 208
+# # 아래 코드의 실행 결과를 예측하라. (읽기 어려운 코드의 예입니다.)
+
+# # print("A")
+# # def message1() :
+# #     print("B")
+# # print("C")
+# # def message2() :
+# #     print("D")
+# # message1()
+# # print("E")
+# # message2()
+
+# '''
+# A
+# C
+# B
+# E
+# D
+# '''
+
+
+# # 207
+# # 아래 코드의 실행 결과를 예측하라. (읽기 어려운 코드의 예입니다.)
+
+# # print("A")
+
+# # def message() :
+# #     print("B")
+
+# # print("C")
+# # message()
+
+
+# '''
+# A
+# C
+# B
+# '''
+
+
+# # 206
+# # 아래 코드의 실행 결과를 예측하라.
+
+# def message() :
+#     print("A")
+#     print("B")
+
+# message()
+# print("C")
+# message()
+
+
+# # 예측 결과
+# '''
+# A
+# B
+# C
+# A
+# B
+# '''
+
+# 205
+# 아래의 에러가 발생하는 이유에 대해 설명하라.
+# name 'hello' is not defined
+
+# hello()
+# def hello():
+#     print("Hi")
+
+# 함수가 선언되기 이전에 호출을 했기 때문
+
+
+# # 204
+# # "비트코인" 문자열을 100번 화면에 출력하는 print_coins() 함수를 정의하라.
+# def print_coin():
+#     for i in range(100):
+#         print("비트코인")
+
+# print_coin()
+
+
+# # 203
+# # 201번에서 정의한 print_coin 함수를 100번호출하라.
+# def print_coin():
+#     print("비트코인")
+
+# for i in range(100):
+#     print_coin()
+
+
+# 202
+# 201번에서 정의한 함수를 호출하라.
+
+# def print_coin():
+#     print("비트코인")
+
+# print_coin()
+
+
+# # 201
+# # "비트코인" 문자열을 화면에 출력하는 print_coin() 함수를 정의하라.
+
+# def print_coin():
+#     print("비트코인")
+
+
+
+
 # # 200
 # # 리스트에는 3일 간의 ohlc 데이터가 저장돼 있다. 시가에 매수해서 종가에 매도 했을 경우 총 수익금을 계산하라.
 

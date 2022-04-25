7af5d8fc396e0ebb8eef8ef388d1b29ffc86b4a2
willow-92 <david383@naver.com>
2022-04-26 08:54:08 +0900

    daily_commit

--- section 13_2_타이핑 게임 제작, 기본 완성.py
+++ section 13_2_타이핑 게임 제작, 기본 완성.py
@@ -36,35 +36,39 @@ with open('./Python_Basic/resource/word.txt','r') as f:
 
 start = time.time()
 
-# while n <= 5:
-#     random.shuffle(words)
-#     q = random.choice(words)
-#     print("*Question # {} : {}".format(n, q)) # 문제 출력
-#     x = input() # 타이핑 입력
+while n <= 5:
+    random.shuffle(words)
+    q = random.choice(words)
+    print("*Question # {} : {}".format(n, q)) # 문제 출력
+    x = input() # 타이핑 입력
 
-#     print()
+    print()
 
-#     if str(q.strip()) == str(x).strip(): # 입력 확인(공백 제거)
-#         print("Pass!")
-#         cor_cnt += 1
-#     else:
-#         print("Wrong!")
-#     print("\n==================\n")
+    if str(q.strip()) == str(x).strip(): # 입력 확인(공백 제거)
+        print("Pass!")
+        # 정답 소리 재생
+        winsound.PlaySound('./Python_Basic/sound/sound_good.wav', winsound.SND_FILENAME)
+        cor_cnt += 1
+    else:
+        # 오답 소리 재생
+        print("Wrong!")
+        winsound.PlaySound('./Python_Basic/sound/sound_bad.wav', winsound.SND_FILENAME)
+    print("\n==================\n")
 
-#     n += 1 # 다음 문제 전환
+    n += 1 # 다음 문제 전환
 
-# end = time.time() #End Time
-# et = end - start # 총 게임시간
-# et = format(et, ".3f") # 소수 셋째 자리 출력(시간)
+end = time.time() #End Time
+et = end - start # 총 게임시간
+et = format(et, ".3f") # 소수 셋째 자리 출력(시간)
 
-# if cor_cnt >= 3:
-#     print("합격")
-# else:
-#     print("불합격")
+if cor_cnt >= 3:
+    print("합격")
+else:
+    print("불합격")
 
-# # 수행 시간 출력
-# print("게임 시간 : ", et, "초", "정답 개수 : {}".format(cor_cnt))
+# 수행 시간 출력
+print("게임 시간 : ", et, "초", "정답 개수 : {}".format(cor_cnt))
 
-# # 시작 지점
-# if __name__ == '__main__':
-#     pass
+# 시작 지점
+if __name__ == '__main__':
+    pass

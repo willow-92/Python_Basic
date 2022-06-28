bb8f06a6783bc89d6bf8c584f9b674621973c042
willow-92 <david383@naver.com>
2022-06-29 08:46:23 +0900

    daily_commit

--- Crawling_Basic/chapter.04-3 Requests 사용 스크래핑(C).py
+++ Crawling_Basic/chapter.04-3 Requests 사용 스크래핑(C).py
@@ -49,16 +49,53 @@ jar = requests.cookies.RequestsCookieJar()
 jar.set('name', 'niceman', domain = "httpbin.org", path='/cookies')
 
 # 요청
-r = s.get('http://httpbin.org/cookies', cookies = jar)
+# r = s.get('http://httpbin.org/cookies', cookies = jar)
 
 # 출력
 # print(r.text)
 
 # 예제3
-r = s.get('https://github.com', timeout=5)
+# r = s.get('https://github.com', timeout=5)
 
 # 출력
 # pprint.pprint(r.text)
 
 # 예제4
-r = s.post('http://httpbin.org/post', data={'id':'test7', 'pw': '111'}, )
+# r = s.post('http://httpbin.org/post', data={'id':'test7', 'pw': '111'}, cookies=jar)
+
+# print(r.text)
+# print(r.headers)
+
+
+# 예제5
+
+payload1 = {'id':'test7', 'pw': '111'}
+payload2 = (('id', 'test7'), ('pw', '111111'))
+
+# r = s.post('http://httpbin.org/post', data=payload2)
+
+# 출력
+# print(r.text)
+
+
+# 예제6(PUT)
+# r = s.put('http://httpbin.org/put', data=payload1)
+
+# print(r.text)
+r = s.delete('http://httpbin.org/delete', data={'id' : 1})
+
+# 출력
+# print(r.text)
+
+# 예제7(Delete)
+r = s.delete('https://jsonplaceholder.typicode.com/posts/1')
+print(r.ok)
+print(r.text)
+print(r.headers)
+
+
+s.close()
+
+# rest api url로 자원의 상태 정보를 받는 것
+# 딜리트, 풋. 리퀘스트 패키지 정말 편하다. 
+# 

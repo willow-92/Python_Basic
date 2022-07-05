490d94ab4b74787d01b917471b4ad9cf543021e8
willow-92 <david383@naver.com>
2022-07-06 08:27:37 +0900

    daily_commit

--- Crawling_Basic/practice_code/self_use/crawling_practice1.py
+++ Crawling_Basic/practice_code/self_use/crawling_practice1.py
@@ -13,23 +13,29 @@ import nltk
 print("import finished")
 
 print("requests url")
-url = "https://www.linkedin.com/jobs/search?keywords=UI%2FUX&location=%EC%84%9C%EC%9A%B8%20%EC%9D%B8%EC%B2%9C%20%EC%A7%80%EC%97%AD&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"
+url = "https://www.linkedin.com/jobs/view/3150157200"
 page = requests.get(url)
 html = page.text
+
 print(html)
 
-print("start parsing")
-soup = BeautifulSoup(html, 'html.parser')
-title = soup.find('h1').string
-company = soup.find('a', class_='job-card-container__link job-card-container__company-name ember-view')
-body_string = ''
-print(soup)
+# print("start parsing")
+# soup = BeautifulSoup(html, 'html.parser')
+# title = soup.find('h1').string
+# company = soup.find('a', class_='ember-view t-black t-normal')
+# content = soup.find('div', class_='jobs-box__html-content jobs-description-content__text t-14 t-normal jobs-description-content__text--stretch')
+# body_string = ''
+# print(title)
+# print(company)
+# print(content)
 
-print("done")
+# print("done")
 
 # 이어서 작업할 것
 # 참조 :https://mokeya.tistory.com/106
 # 어떤 사이트를 긁어와야 하는지 부터 확인할 것
 # 대기
 
+# 동적인 자료는 셀레늄 활용이 필요하다는 글을 발견
+# 다시 확인해볼 것
 

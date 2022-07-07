833ea12be4f84781e8c6d768abf1c4b60dbe9750
willow-92 <david383@naver.com>
2022-07-08 08:41:29 +0900

    daily_commit

--- Crawling_Basic/practice_code/self_use/crawling_practice1.py
+++ Crawling_Basic/practice_code/self_use/crawling_practice1.py
@@ -26,18 +26,18 @@ page = requests.get(url, headers=headers)
 html = page.text
 
 
-print(html)
+# print(html)
 
 
-# print("start parsing")
-# soup = BeautifulSoup(html, 'html.parser')
-# title = soup.find('h1').string
-# company = soup.find('a', class_='ember-view t-black t-normal')
-# content = soup.find('div', class_='jobs-box__html-content jobs-description-content__text t-14 t-normal jobs-description-content__text--stretch')
-# body_string = ''
-# print(title)
-# print(company)
-# print(content)
+print("start parsing")
+soup = BeautifulSoup(html, 'html.parser')
+title = soup.find('h1').string
+company = soup.find('span', class_='jobs-unified-top-card__company-name')
+content = soup.find('div', class_='jobs-box__html-content jobs-description-content__text t-14 t-normal jobs-description-content__text--stretch')
+body_string = ''
+print(title)
+print(company)
+print(content)
 
 # print("done")
 
@@ -49,3 +49,5 @@ print(html)
 # 동적인 자료는 셀레늄 활용이 필요하다는 글을 발견
 # 다시 확인해볼 것
 
+# 우선 requests를 통해서 받아오는 html 안에 똑같은 태그 이름을 가진 텍스트가 존재하지 않음.
+# 개발자 도구에서는 보이는데 requests로 가져오는 html에는 왜 보이지 않는지 확인할 필요가 있음.

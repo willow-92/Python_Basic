756f1d98bb56d09c6733c7aefbd8f425d1522916
kyung.song <david383@naver.com>
2022-07-20 08:41:42 +0900

    daily_commit

--- Crawling_Basic/practice_code/chapter.05-1 Beautiful Soup 사용 스크래핑.py
+++ Crawling_Basic/practice_code/chapter.05-1 Beautiful Soup 사용 스크래핑.py
@@ -30,5 +30,18 @@ html = """
 # 예재1(Beautifulsoup 기초)
 # bs4 초기화
 soup = BeautifulSoup(html, 'html.parser')
-print(type(soup))
-print('prettify', soup.pretify)
+# print(type(soup))
+# print('prettify', soup.prettify())
+
+# h1 태그 접근
+h1 = soup.html.body.h1
+print('h1', h1)
+
+#p 태그 접근
+# body 안에 p 태그가 3개 있지만, 아래 방식으로 접근하면 첫 번째 자식인 p 태그를 가지고 옴
+p1 = soup.html.body.p
+print('p1', p1)
+
+# 다음 태그
+p2 = p1.next_sibling.next_sibling
+print('p2', p2)
